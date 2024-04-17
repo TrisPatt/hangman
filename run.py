8aee1b6bf40b7d2a8713cc47790374eb3f68bae1
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-04-17 15:57:19 +0000

    docs: run through spellchecker

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from words import words_list
 
 def rules():
     """
-    Display the rules of the game and return to the main menu
+    Display the rules of the game and return to the main menu.
     """
     clear_screen()
     print("----------------------------------------------------------------")
@@ -16,13 +16,13 @@ def rules():
     print("""
     Hangman is a word guessing game. The aim is to guess the hidden 
     word before your lives run out.
-    Guess a letter. If the letter is in the hidden word then this will be 
-    displayed in the position it is in that word. If it is not in the word 
-    then you will lose a life and another part of the hangman will be revealed. 
-    The previous guessed letters will be displayed. 
+    Guess a letter. If the letter is in the hidden word, it will be 
+    displayed in the position it occupies in that word. If it is not in the word, 
+    you will lose a life, and another part of the hangman will be revealed. 
+    The previously guessed letters will be displayed. 
     You have 6 lives. 
-    If you get 6 incorrect answers then you lose.
-    If you guess the word before the hangman is complete then you win!
+    If you make 6 incorrect guesses, you lose.
+    If you guess the word before the hangman is complete, you win!
     The word will be the name of a movie title""")
     print("----------------------------------------------------------------")
     print("Press ENTER to return to the main menu...")
@@ -33,7 +33,7 @@ def rules():
 def print_gallows(incorrect_guesses, max_incorrect_guesses):
     """
     Prints the status of the gallows taken from the list in gallows.py
-    depending on the number of incorrect guesses
+    depending on the number of incorrect guesses.
     """
     if incorrect_guesses < len(gallows_image):
         print(gallows_image[incorrect_guesses])
@@ -43,8 +43,8 @@ def print_gallows(incorrect_guesses, max_incorrect_guesses):
 
 def clear_screen():
     """
-    Clear the terminal after each guess. The OS module is imported which 
-    allows for the function to operate on different operating systems.
+    Clears the terminal after each guess. The OS module is imported, allowing
+    the function to operate on different operating systems.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -52,7 +52,7 @@ def clear_screen():
 def return_main_menu():
     """
     While loop with a question returning 2 possible answers and error handling.
-    Converts the input to lowercase
+    Converts the input to lowercase.
     """
     while True:
         menu_return = input("""Would you like to return to the main menu? y/n: \n""").lower()
@@ -65,12 +65,13 @@ def return_main_menu():
             exit()
             break
         else:
-            print("please press 'y' or 'n'")
+            print("Please press 'y' or 'n'")
 
 def multiple_letter(word, letter):
     """
-    Iterates over each character in the word using a for loop combined with enumerate to 
-    access both the character and its index in the word.
+    Iterates over each character in the word using a for loop combined with enumerate
+    to access both the character and its index in the word.
+
     Returns a list containing the indices (positions) where the letter occurs in the word.
     If the letter does not occur in the word, an empty list is returned.
     """
@@ -111,7 +112,7 @@ def start_game():
         guess = input("Enter your guess: \n").upper()
         clear_screen()
 
-        #checks input is not more than one character or not alphabetic
+        # Check if the input is not more than one character or not alphabetic.
         if len(guess) != 1 or not guess.isalpha():
             print("Invalid option. Please enter a single letter\n")
             print_gallows(incorrect_guesses, max_incorrect_guesses)
@@ -119,7 +120,7 @@ def start_game():
             print("The word to guess is:", " ".join(hidden_letters)) 
             continue   
 
-        #checks if the input is in the previous guesses list
+        # Check if the input is in the previous guesses list.
         if guess in previous_guesses:
             print("You've already guessed that!\n")
             print("The word to guess is:", " ".join(hidden_letters))
@@ -128,7 +129,7 @@ def start_game():
             continue
 
         """ Iterate through each index of the hidden word to check for 
-        multiple occurrences of the guessed letter """
+        multiple occurrences of the guessed letter. """
         if guess in hidden_word:
             print("Correct!!\n")
             multiple = multiple_letter(hidden_word, guess)
@@ -136,7 +137,7 @@ def start_game():
                 hidden_letters[index] = guess
             print(f"The word to guess is:", " ".join(hidden_letters))
 
-            #check if all letters have been guessed correctly
+            # Check if all letters have been guessed correctly.
             if "_" not in hidden_letters: 
                 print("Congratulations! You've guessed the word correctly!\n")
                 return_main_menu()
@@ -152,7 +153,7 @@ def start_game():
             previous_guesses.append(guess)
             print(f"Incorrect guesses: ", " ".join(previous_guesses))
             
-            #maximum guesses reached
+            # Check if maximum guesses reached.
             if incorrect_guesses > max_incorrect_guesses:
                 print("You lost! You ran out of guesses.\n")
                 print(f"The word you were looking for is {hidden_word} !")
@@ -163,7 +164,8 @@ def start_menu():
     """
     Creates the introduction and provides 3 options to the user with a 
     while loop to limit the input to the 3 choices only.
-    provides a way of exiting the game, rules and staring the game
+    
+    Provides a way of exiting the game, rules, and starting the game.
     """
     print("-----------------------------------")
     print("Welcome to Hangman! - Movie edition")
@@ -198,5 +200,5 @@ def start_menu():
 
     
 start_menu()
-  
+
 
